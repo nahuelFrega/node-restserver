819909d3576f20585ac1ae3af9b1ad032e4343ed
Nahuel <nahuel.frega@gmail.com>
2020-04-18 00:21:00 -0300

    Segundo commit

---
+++ server/routes/usuario.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const Usuario = require('../models/usuario');
+const app = express();
+const bcrypt = require('bcrypt');
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+
+    });
+
+    usuario.save((err, usuarioDB) => {
+
+        if (err) {
+
+            return res.status(400).json({
+                ok: false,
+                err,
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioDB,
+        })
+
+    });
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+
+// Exports
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
-
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,49 +11,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Es necesario ingrear el nombre'
-        });
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
+// Conexión DB
+mongoose.connect(`mongodb://localhost:27017/cafe`, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(`DB Connection Error: $ { err.message }`);
     });
 
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
-
-
-
+mongoose.set('useCreateIndex', true);
 
 // Listen
 app.listen(process.env.PORT, () => {
