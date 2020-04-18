b522afdc13baff0b5d19f684e75fb46a31b8a706
Nahuel <nahuel.frega@gmail.com>
2020-04-18 17:12:11 -0300

    Mejoras

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'dev') {
 
 } else {
 
-    urlDB = 'mongodb+srv://root:cbr3f100@cluster0-rkkfk.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
