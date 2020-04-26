83d0736d975f2763bae84c46b1c978873b76740f
Nahuel <nahuel.frega@gmail.com>
2020-04-25 23:47:37 -0300

    Ultima version upload imagen

--- server/middlewares/authenticacion.js
+++ server/middlewares/authenticacion.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // ====================
-// Verifica el Token:
+// Verifica el Token enviado por HEADER:
 // Permite controlar las distintas acciones a partir de requerir un token válido 
 // ====================
 let verificaToken = (req, res, next) => {
@@ -46,9 +46,36 @@ let verificaAdminRol = (req, res, next) => {
 
 }
 
+// ====================
+// Verifica el Token enviado por PARAMS:
+// Permite controlar las distintas acciones a partir de requerir un token válido 
+// ====================
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    // Valida el token
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                message: 'Token no válido'
+            })
+        }
+
+        // Vuelca los datos 'decoded' a req.usuario
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+};
+
 
 
 module.exports = {
     verificaToken,
-    verificaAdminRol
+    verificaAdminRol,
+    verificaTokenImg
 };
--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -3,21 +3,28 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const { verificaToken, verificaTokenImg } = require('../middlewares/authenticacion');
 
 
-app.get('/imagen/:entidad/:img', (req, res) => {
+app.get('/imagen/:entidad/:img', verificaTokenImg, (req, res) => {
 
     let entidad = req.params.entidad;
     let img = req.params.img;
+    let pathImagen = path.resolve(__dirname, `../../uploads/${ entidad }/${ img }`);
 
-    let pathImg = `./uploads/${ entidad }/${ img }`;
+    // Comprueba que exista el path. Por ende que exista la imagen que se esta buscando
+    if (fs.existsSync(pathImagen)) {
 
-    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+        res.sendFile(pathImagen);
 
-    res.sendFile(noImagePath);
+    } else {
+
+        let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+        res.sendFile(noImagePath);
+
+    }
 
 });
-
 
 
 module.exports = app;
